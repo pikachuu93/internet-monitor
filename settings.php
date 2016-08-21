123aea2c3b17ac2e9ad247182bbf9a16643af762
Dan Young <pi@localhost.localdomain>
2016-08-21 23:17:48 +0000

    Towards speed test functionality

--- settings.php
+++ settings.php
@@ -2,8 +2,13 @@
 
 class Settings
 {
-  public static $root = "/var/www/html/";
-  public static $dbPath = "/home/pi/connectivity.sqlite";
+  public static $root          = "/var/www/html/";
+  public static $dbPath        = "/home/pi/connectivity.sqlite";
+
+  /* Need speed test script from https://github.com/sivel/speedtest-cli */
+  public static $haveSpeedTest = true;
+  public static $lockFile      = "/var/www/html/speed-test.lock";
+  public static $speedTest     = "/var/www/html/speedtest_cli.py";
 }
 
 ?>
