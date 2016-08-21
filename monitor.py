153bb4269c301595d26e1cade010d7e4d0707a5f
Dan Young <pi@localhost.localdomain>
2016-08-21 10:29:52 +0000

    Adding monitor.py

---
+++ monitor.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import subprocess, time, datetime, sys, sqlite3
+
+class Monitor():
+  def __init__(self, args = []):
+    # Defaults will be overwritten by parseArgs
+    self.freq    = 60
+    self.retrys  = 3
+    self.timeout = 5
+    self.address = "8.8.8.8"
+    self.path    = "/home/pi/"
+    self.dbName  = "connectivity.sqlite"
+
+    self.now = datetime.datetime.now()
+
+    self.connection = sqlite3.connect(self.path + self.dbName)
+
+    self.parseArgs(args)
+
+    self.sleep()
+
+    self.run()
+
+  def run(self):
+    while True:
+      self.now = datetime.datetime.now()
+
+      res = self.checkConnection()
+
+      self.saveResult(res)
+
+      self.sleep()
+
+  def checkConnection(self):
+    for i in range(self.retrys):
+      res = subprocess.call(["/bin/ping", "-c1", "-w" + str(self.timeout), self.address],
+                stdout = subprocess.PIPE)
+      if not res:
+        return 1
+
+    return 0
+
+  def saveResult(self, res):
+    c = self.connection.cursor()
+
+    c.execute("INSERT INTO connected (datetime, value) VALUES ("
+                        + str(int(time.mktime(self.now.timetuple())))
+                        + ", " + str(int(res)) + ");")
+
+    self.connection.commit()
+
+  def sleep(self):
+    delta  = datetime.timedelta(minutes = 1)
+    future = self.now + delta
+
+    future = future.replace(second = 0, microsecond = 0)
+
+    sleep = (future - self.now).total_seconds()
+
+    if sleep > 0:
+      time.sleep(sleep)
+
+  def parseArgs(self, argsIn):
+    args = list(argsIn)
+
+m = Monitor()
