8e0b4567ac5a953776a17679fabe65bf4f7fb96c
Dan Young <daniel1993@live.co.uk>
2016-09-01 18:05:36 +0000

    Adding overview page to get general db stats.

---
+++ pages/overview.php
@@ -0,0 +1,36 @@
+<?php
+
+class Overview extends Frame
+{
+  protected $name = "Overview";
+  protected $url  = "overview";
+
+  public function load()
+  {
+    $qs = ["Number of rows" => "SELECT COUNT(*) FROM connected;",
+           "Last Outage"    => "SELECT datetime(datetime, 'unixepoch') "
+                             . "FROM connected WHERE value=0 ORDER BY datetime DESC LIMIT 1;",
+           "Total Outage"   => "SELECT COUNT(*) FROM connected WHERE value=0;",
+           "Total Uptime"   => "SELECT COUNT(*) FROM connected WHERE value=1;"];
+
+    $html = "<table>";
+    foreach ($qs as $key => $q)
+    {
+      $html .= "<tr>";
+
+      $res = Page::$db->query($q);
+
+      $html .= "<td>$key</td><td>" . $res->fetchArray()[0] . "</td>";
+
+      $html .= "</tr>";
+    }
+
+    $html .= "</table>";
+
+    Page::addBody($html);
+  }
+}
+
+return new Overview;
+
+?>
